066361b6fb77fd1e975abd90b8bf234d9477943e
ADHIRAJPS1 <adhirajpratap101@gmail.com>
2023-04-05 21:38:20 +0530

    server setup

---
+++ src/server.js
@@ -0,0 +1,14 @@
+const dotenv = require('dotenv');
+const express = require('express');
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT;
+
+app.get('/', (req, res) => {
+  res.send('Express + TypeScript Server');
+});
+
+app.listen(port, () => {
+  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+});
